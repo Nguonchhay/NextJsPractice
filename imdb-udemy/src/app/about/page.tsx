d18e79eba4cae6e14d9bad08d673de0138872ddc
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2023-09-19 23:54:44 +0700

    [TASK] Add about page

---
+++ imdb-udemy/src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>
+            You can specify how many words should be generated right next to the word "lorem". For example, lorem5 will generate a 5-words dummy text.
+            Note 1: This extension only works on input fields and textareas (this means TinyMCE visual field does not work, but works on the Text view).
+            Note 2: You can freely hide the icon of this extension, if you have mastered the Lorem ipsum generation.
+            You can specify how many words should be generated right next to the word "lorem". For example, lorem5 will generate a 5-words dummy text.
+            Note 1: This extension only works on input fields and textareas (this means TinyMCE visual field does not work, but works on the Text view).
+            Note 2: You can freely hide the icon of this extension, if you have mastered the Lorem ipsum generation.
+            You can specify how many words should be generated right next to the word "lorem". For example, lorem5 will generate a 5-words dummy text.
+            Note 1: This extension only works on input fields and textareas (this means TinyMCE visual field does not work, but works on the Text view).
+            Note 2: You can freely hide the icon of this extension, if you have mastered the Lorem ipsum generation.
+        </p>
+    </div>
+  )
+}
